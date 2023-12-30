569bb6e3fbc8ce9eeb59c2d5eca72247924aa818
hassan <hassanajibade17@gmail.com>
2023-12-30 15:20:01 -0800

    mobile header

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,10 +1,12 @@
 // import React from 'react'
 import Header from "./Components/Header"
+import Home from "./Components/Home"
 import './SCSS/Header.scss'
 const App = () => {
   return (
     <div>
       <Header/>
+      <Home/>
     </div>
   )
 }
--- portfolio/src/Components/Header.tsx
+++ portfolio/src/Components/Header.tsx
@@ -1,6 +1,11 @@
-// import {useState} from 'react'
+import {useState} from 'react'
 
 const Header = () => {
+  const [menu, setMenu] = useState(false)
+
+  const Dropdown=(): void => {
+    menu? setMenu(false): setMenu(true)
+  }
   return (
     <div className="Header_container">
         <span className="Header_Logo">
@@ -11,6 +16,19 @@ const Header = () => {
             <li>Projects</li>
             <li>Contact</li>
         </ul>
+
+        <div className="Header_Menu" onClick={Dropdown}>Menu</div>
+        {
+          menu? (
+        <ul className="Mobile_Header_list">
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+          ) : null
+        }
+        
+
     </div>
   )
 }
