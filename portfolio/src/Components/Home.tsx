f15e2d1a11216af1dacd8092167bdcf499cfc90b
hassan <hassanajibade17@gmail.com>
2023-12-30 16:04:56 -0800

    starting Homepage

--- portfolio/src/Components/Home.tsx
+++ portfolio/src/Components/Home.tsx
@@ -2,7 +2,19 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="Home_container">
+      <span className="Social_icons">
+
+      </span>
+      <img src="" alt="" />
+      <article>
+        <h1>Hi, I'm Ajibade Hassan</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+          Neque odit labore doloribus in alias ea pariatur laborum,
+          officiis saepe? Pariatur aliquam eius praesentium impedit
+           ipsam eaque voluptatem, assumenda quae ex.</p>
+      </article>
+    </div>
   )
 }
 
