da731956509d6ebce59d9dc3c2aaa1106a69e6e5
hassan <hassanajibade17@gmail.com>
2024-01-12 22:41:46 +0100

    delayed commit

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -2,6 +2,7 @@
 import Header from "./Components/Header"
 import Home from "./Components/Home"
 import './SCSS/Header.scss'
+import './SCSS/Home.scss'
 const App = () => {
   return (
     <div>
--- portfolio/src/Components/Home.tsx
+++ portfolio/src/Components/Home.tsx
@@ -3,11 +3,13 @@
 const Home = () => {
   return (
     <div className="Home_container">
-      <span className="Social_icons">
+     <article className="Home_media">
+     <span className="Social_icons">
 
-      </span>
-      <img src="" alt="" />
-      <article>
+</span>
+<img src="" alt="" />
+     </article>
+      <article className="Home_text">
         <h1>Hi, I'm Ajibade Hassan</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
           Neque odit labore doloribus in alias ea pariatur laborum,
