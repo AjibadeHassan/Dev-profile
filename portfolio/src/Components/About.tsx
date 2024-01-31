b3bfc935c232488bd68af745b7760a2cdfbb9050
hassan <hassanajibade17@gmail.com>
2024-01-31 02:43:50 +0100

    last in january

--- portfolio/src/Components/About.tsx
+++ portfolio/src/Components/About.tsx
@@ -3,7 +3,18 @@
 const About = () => {
   return (
     <div className='About_Container'>
-        
+        <section className="About_Header">
+          <h1>ABOUT ME</h1>
+          <span></span>
+          <p>Here you will find more information about me,
+             what I do, and my current skills mostly in terms
+              of programming and technology</p>
+        </section>
+        <section className="About_body">
+          <div className="About_details">
+            <h2>Get To Know Me!</h2>
+          </div>
+        </section>
     </div>
   )
 }
