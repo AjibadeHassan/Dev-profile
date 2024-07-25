2cd7f3995d2418875865d7b357ef4b052be4442b
hassan <hassanajibade17@gmail.com>
2024-07-25 23:21:59 +0100

    footer addition

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -4,12 +4,14 @@ import Home from "./Components/Home"
 import About from "./Components/About"
 import Projects from "./Components/Projects"
 import Contacts from "./Components/contacts."
+import Footer from "./Components/Footer"
 import './SCSS/Header.scss'
 import './SCSS/Home.scss'
 import './SCSS/About.scss'
 import './SCSS/Projects.scss'
 import './SCSS/Mobile-projects.scss'
 import './SCSS/Contact.scss'
+import './SCSS/Footer.scss'
 import './SCSS/Mobile-contacts.scss'
 const App = () => {
   return (
@@ -19,6 +21,7 @@ const App = () => {
       <About/>
       <Projects/>
       <Contacts/>
+      <Footer/>
     </div>
   )
 }
---
+++ portfolio/src/Components/Footer.tsx
@@ -0,0 +1,20 @@
+
+const Footer = () => {
+  return (
+    <div className="Footer_Container">
+        <div className="Footer_Body">
+            <div className="Footer_Top">
+                <span>
+                    <h1>AJIBADE HASSAN</h1>
+                    <p>
+                    I'm a MERN Stack Web Developer building the Front-end and Back-end of projects.</p>
+                </span>
+                <span></span>
+            </div>
+            <div className="Footer_Bottom">© Copyright 2024 . Made by Ajibade Hassan</div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
