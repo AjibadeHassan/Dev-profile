147cbba25e0ce12d82c373ca37936dd32ab45422
hassan <hassanajibade17@gmail.com>
2024-07-26 23:00:39 +0100

    final touch

--- portfolio/src/Components/Footer.tsx
+++ portfolio/src/Components/Footer.tsx
@@ -1,3 +1,5 @@
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
@@ -9,7 +11,12 @@ const Footer = () => {
                     <p>
                     I'm a MERN Stack Web Developer building the Front-end and Back-end of projects.</p>
                 </span>
-                <span></span>
+                <span className="Social_Container">
+                  <h1>SOCIAL</h1>
+                  <span className="Social_Icons"><FaXTwitter/></span>
+      <span className="Social_Icons"><FaLinkedin/></span>
+      <span className="Social_Icons"><FaGithub/></span>
+                </span>
             </div>
             <div className="Footer_Bottom">© Copyright 2024 . Made by Ajibade Hassan</div>
         </div>
