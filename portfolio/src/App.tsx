ed8ecdbb02c441ab05362b097de0bacbc06f6edd
Melvin-Emmanuel <emmanulmelv@gmail.com>
2025-04-15 18:44:23 +0100

    optimising code

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,34 +1,44 @@
-// import React from 'react'
-import Header from "./Components/Header"
-import Home from "./Components/Home"
-import About from "./Components/About"
-import Projects from "./Components/Projects"
-import Contacts from "./Components/contacts."
-import Footer from "./Components/Footer"
-import './SCSS/Header.scss'
-import './SCSS/Home.scss'
-import './SCSS/About.scss'
-import './SCSS/Projects.scss'
-import './SCSS/Mobile-projects.scss'
-import './SCSS/Contact.scss'
-import './SCSS/Footer.scss'
-import './SCSS/Mobile-contacts.scss'
-import { Route, Routes } from "react-router-dom"
+import React from 'react'
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Projects from "./Components/Projects";
+import Contacts from "./Components/contacts.";
+import Footer from "./Components/Footer";
+
+import "./SCSS/Header.scss";
+import "./SCSS/Home.scss";
+import "./SCSS/About.scss";
+import "./SCSS/Projects.scss";
+import "./SCSS/Mobile-projects.scss";
+import "./SCSS/Contact.scss";
+import "./SCSS/Footer.scss";
+import "./SCSS/Mobile-contacts.scss";
+
 const App = () => {
   return (
     <div>
-      <Header/>
-      <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/projects" element={<Projects/>}/>
-          <Route path="/contacts" element={<Contacts/>}/>
-      </Routes>
-      <Footer/>
+      <Header />
 
+      <section id="home">
+        <Home />
+      </section>
 
+      <section id="about">
+        <About />
+      </section>
+
+      <section id="projects">
+        <Projects />
+      </section>
+
+      <section id="contacts">
+        <Contacts />
+      </section>
+
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
